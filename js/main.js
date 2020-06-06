75d446a2b4ea6526943c9e12fb7a41034b65a97f
Roman <roman.g.demyancuk@gmail.com>
2020-06-06 16:34:46 +0300

    Footer and Adaptive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 $(function() {
-  // $('.slider_inner').slick({
-  //     nextArrow: '<button type="button" class=" slick-btn slick-next"></button>',
-  //   prevArrow: '<button type="button" class=" slick-btn slick-prev"></button>',
-  //     infinite:false
-  //   });
     $('.slider_inner').slick({
       infinite:true,
       slidesToShow:10,
@@ -11,5 +6,8 @@ $(function() {
       nextArrow: '<button type="button" class=" slick-btn slick-next"><i class="fas fa-arrow-right"></i></button>',
       prevArrow: '<button type="button" class=" slick-btn slick-prev"><i class="fas fa-arrow-left"></i></button>',
       arrows:true
-     });    
+     });
+    $('.menu-btn').on('click', function () {
+        $('.menu').slideToggle();
+    });
 });
