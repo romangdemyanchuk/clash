a507a9a1a2710a778b169df497f357199ff96bb4
Roman <roman.g.demyancuk@gmail.com>
2020-06-08 01:46:11 +0300

    Adaptive

--- js/main.js
+++ js/main.js
@@ -5,7 +5,30 @@ $(function() {
       slidesToScroll: 1,
       nextArrow: '<button type="button" class=" slick-btn slick-next"><i class="fas fa-arrow-right"></i></button>',
       prevArrow: '<button type="button" class=" slick-btn slick-prev"><i class="fas fa-arrow-left"></i></button>',
-      arrows:true
+      arrows:true,
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+            // {
+            //     breakpoint: 1000,
+            //     settings: {
+            //         arrows: false,
+            //         centerMode: true,
+            //         centerPadding: '150px',
+            //         slidesToShow: 1
+            //     }
+            // }
+            ]
      });
     $('.menu-btn').on('click', function () {
         $('.menu').slideToggle();
